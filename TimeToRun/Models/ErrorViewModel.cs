c9d758754af9bbce7b3d9b353e0df9b359a3f0c2
Wajahat <wajahat.mir1@gmail.com>
2024-01-29 20:47:47 -0500

    Add project files.

---
+++ TimeToRun/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TimeToRun.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
