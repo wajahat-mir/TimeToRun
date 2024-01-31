6e51814d6638755e11defcf6384c1529e9edd8b9
Wajahat <wajahat.mir1@gmail.com>
2024-01-30 22:15:02 -0500

    UI

---
+++ TimeToRun/Views/Home/Components/DayForecast/DayForecast.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeToRun.Views.Home.Components.DayForecast
+{
+    public class DayForecast : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
