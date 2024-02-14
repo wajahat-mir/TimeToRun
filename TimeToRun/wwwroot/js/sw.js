6a3dd5084a4b9e729160a7eea30236c192a6266b
Wajahat <wajahat.mir1@gmail.com>
2024-02-13 23:22:49 -0500

    Add basic PWA

---
+++ TimeToRun/wwwroot/js/sw.js
@@ -0,0 +1,49 @@
+﻿const VERSION = "v1";
+const CACHE_NAME = `time-to-run-{VERSION}`;
+
+const APP_STATIC_RESOURCES = [
+    "/js/site.js",
+    "/css/site.css"
+]
+
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        (async () => {
+            const cache = await caches.open(CACHE_NAME);
+            cache.addAll(APP_STATIC_RESOURCES);
+        })(),
+    );
+});
+
+self.addEventListener("activate", (event) => {
+    event.waitUntil(
+        (async () => {
+            const names = await caches.keys();
+            await Promise.all(
+                names.map((name) => {
+                    if (name !== CACHE_NAME) {
+                        return caches.delete(name);
+                    }
+                }),
+            );
+            await clients.claim();
+        })(),
+    );
+});
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        (async () => {
+            const r = await caches.match(e.request);
+            console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+            if (r) {
+                return r;
+            }
+            const response = await fetch(e.request);
+            const cache = await caches.open(cacheName);
+            console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+            cache.put(e.request, response.clone());
+            return response;
+        })(),
+    );
+});
